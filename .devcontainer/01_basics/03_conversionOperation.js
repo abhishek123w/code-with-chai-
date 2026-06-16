deeef8167496a75a3ada361e151938a36c9308e1
abhishek123w <pawarsingh817142@gmail.com>
2026-06-16 07:18:18 +0000

    comparision of dataTypes

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -10,3 +10,39 @@ console.log(valueInNumber);
 
 
 
+
+// *********************************** Operation ****************************
+
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// leconsole.log(2%3);
+
+let str1 = "hello"
+let str2 = "Abhishek"
+
+let str3  = str1 + str2
+console.log("1"+ 2);
+console.log(1+"2");
+console.log("1"+2+2);
+
+
+// console.log( (3+4) * 5%3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,31 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log("200" >= 200);
+console,log("100" < 300);
+console.log(null === 0);
+
+
+console.log(undefind == 0);
+console,log(undefind > 0);
+console.log(undefind < 0);
+
+
+// ===
+
+console.log("2" === 2);
+
+
+
