712354b704b7feef7eee7562898f434e6934603a
abhishek123w <pawarsingh817142@gmail.com>
2026-06-18 06:36:05 +0000

    datatypes summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,48 @@
+// Primitive
+
+// 7 types : String , Number, Boolearn , null , undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const inLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 82299292255555556555n
+
+// Refrence (Non primitive)
+
+// Array, objects , Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    
+    name: "Abhishek",
+
+    age: 20,
+
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+
+
+}
+
+console.log(typeof bigNumber);
+
+
+
+
+// Refrence (Non primitive)
+
+
+
+// Array , Objects , Functions
