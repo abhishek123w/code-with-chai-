ba80a32cf28ddbc04fcea966d331d7a521897ba0
abhishek123w <pawarsingh817142@gmail.com>
2026-07-01 14:23:59 +0000

    math and num

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,42 @@
+const name = "abhishek"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('abhishek')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+
+console.log(gameName.chartAt(2));
+console.log(gameName.indexOf('t'));
+
+
+const newString = gameName.substring(0 ,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newString = "   abhishek   "
+console.log(newStringOne);
+console.log(newStringOne.train());
+
+const url = "http://abhishekbhuiyar8@gmail.com"
+
+console.log(url.replace('20', '_'))
+
+console.log(url.includes('abhishek'))
+
+console.log(gameName.split('_'));
+
+
+
+
+
